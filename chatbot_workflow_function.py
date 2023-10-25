8453f002a22325a1f85295caa4b5b0c9e1978e0b
hieuhuynhcao <hieuhuynhcao.com>
2023-10-25 11:54:17 +0700

    update

--- chatbot_workflow_function.py
+++ chatbot_workflow_function.py
@@ -1,21 +1,16 @@
 import fitz  # import package PyMuPDF
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain, RetrievalQA
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.memory import ConversationBufferMemory
-from langchain.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate, PromptTemplate
+from langchain.chains import RetrievalQA, LLMChain
+from langchain.prompts import PromptTemplate
 from langchain.text_splitter import SpacyTextSplitter, RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma, FAISS
-import streamlit as st
 import pickle5 as pickle
 
-from html_template import bot_template, user_template
-from resources import load_vector_store, vectorstore
-from utils import flatten_matrix, deduplicated_documents
+from resources import vectorstore
+from utils import flatten_matrix, deduplicated_documents, LineListOutputParser
+from langchain.retrievers.multi_query import MultiQueryRetriever
 
-
-#Load docs
-def get_docs(filename, mem_area):
+#Load pdf docs: not use
+def _get_docs(filename, mem_area):
     docs = fitz.open(filename, stream=mem_area, filetype="pdf")
     return docs
 
@@ -32,11 +27,12 @@ def split_docs_with_recursive(docs,metadata, chunk_size=2000):
                                                        metadatas=[metadata])
     return all_splits
 
-def split_docs(docs, metadata, chunk_size=4000):
+# Not use
+def _split_docs(docs, metadata, chunk_size=4000):
     text_splitter = SpacyTextSplitter(pipeline='en_core_web_trf'
                                       ,separator='\n\n'
                                       ,chunk_size=chunk_size
-                                      ,chunk_overlap=500)
+                                      ,chunk_overlap=0)
     all_splits = text_splitter.create_documents([docs]
                                                 ,metadatas = [metadata])
     return all_splits
@@ -48,6 +44,36 @@ def store_docs_to_vectorstore(all_splits):
         pickle.dump(vectorstore, f)
 
 #Generate
+def create_exam_questions(question):
+    CREATE_EXAMPLE_QUESTIONS_PROMPT = PromptTemplate(
+        input_variables=["question"],
+        template="""
+        Act as a Prompt Enhancer AI that takes user-input prompts and transforms them into more engaging, detailed, and thought-provoking questions. Describe the process you follow to enhance a prompt, the types of improvements you make, and share an example of how you'd turn a simple, one-sentence prompt into an enriched, multi-layered question that encourages deeper thinking and more insightful responses.
+        Your task is to generate three different versions of the given user question to retrieve relevant documents from a vector 
+        database. By generating multiple perspectives on the user question, your goal is to help
+        the user overcome some of the limitations of the distance-based similarity search. 
+        Provide these alternative questions separated by newlines.
+        Original question: {question}""",
+    )
+    # Chain
+    llm_chain = LLMChain(
+                        llm = ChatOpenAI(temperature = 0
+                                    ,model = "gpt-3.5-turbo"
+                                    ,max_tokens = 1000)
+                         ,prompt = CREATE_EXAMPLE_QUESTIONS_PROMPT
+                         ,output_parser = LineListOutputParser())
+
+    # Run
+    retriever = MultiQueryRetriever(
+        retriever = vectorstore.as_retriever(), 
+        llm_chain = llm_chain, 
+        parser_key = "lines"
+    )  # "lines" is the key (attribute name) of the parsed output
+
+    # Results
+    unique_docs = retriever.get_relevant_documents(query=question)
+    return unique_docs
+
 def create_paraphrase_questions(user_question):
     qa_chain = RetrievalQA.from_chain_type(
             llm = ChatOpenAI(model_name="gpt-3.5-turbo",
@@ -80,15 +106,15 @@ def generate_conversation_chain(question, vectorstore):
     )
     conversation_chain = RetrievalQA.from_chain_type(
             llm = ChatOpenAI(temperature = 0
-                            ,model = "gpt-4"
-                            ,max_tokens = 1500)
+                            ,model = "gpt-3.5-turbo"
+                            ,max_tokens = 1000)
             ,retriever = vectorstore.as_retriever()
             ,chain_type_kwargs={ "prompt": QUERY_PROMPT }
             ,verbose=True
     )
-    print(len(create_paraphrase_questions(question)))
-    result = conversation_chain({'query': question, 
-                                 'context': "".join(doc.page_content for doc in create_paraphrase_questions(question))})
+    #print(len(create_paraphrase_questions(question)))
+    result = conversation_chain({'query': question,
+                                 'context': "".join(doc.page_content for doc in create_exam_questions(question))})
     return result
 
 # def handle_userinput(user_question):
--- utils.py
+++ utils.py
@@ -1,4 +1,18 @@
+from typing import List
+from pydantic import BaseModel, Field
+from langchain.output_parsers import PydanticOutputParser
 
+# Output parser will split the LLM result into a list of queries
+class LineList(BaseModel):
+    # "lines" is the key (attribute name) of the parsed output
+    lines: List[str] = Field(description="Lines of text")
+class LineListOutputParser(PydanticOutputParser):
+    def __init__(self) -> None:
+        super().__init__(pydantic_object=LineList)
+
+    def parse(self, text: str) -> LineList:
+        lines = text.strip().split("\n")
+        return LineList(lines=lines)
 
 def flatten_matrix(two_dimensional_list):
     return [element for sublist in two_dimensional_list for element in sublist]
